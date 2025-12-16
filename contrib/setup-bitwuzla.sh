213c7b0392b150f8a9cae56111583a679f4b86e7
Aina Niemetz <aina.niemetz@gmail.com>
2025-12-16 09:49:48 -0800

    Use new version of Bitwuzla.

--- contrib/setup-bitwuzla.sh
+++ contrib/setup-bitwuzla.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-git_commit=0ef2ebff4f52239a3b7d5309c25b4e535b60a12d
+git_commit=64dee7007aa0092c87dab171ea199c14f3a608a9
 
 download_step() {
-  git clone --revision=$git_commit git@github.com:bitwuzla/bitwuzla-interpolants.git $dep_name
+  git clone --revision=$git_commit git@github.com:aniemetz/bitwuzla-private.git $dep_name
 }
 
-prepare_step() {
-  "$contrib_dir/setup-cadical.sh"
-  patch -p1 <"$contrib_dir/bitwuzla_libgmp.patch"
-}
+# prepare_step() {
+#   "$contrib_dir/setup-cadical.sh"
+#   patch -p1 <"$contrib_dir/bitwuzla_libgmp.patch"
+# }
 
 source "$(dirname "$(realpath "$0")")/meson-steps.sh"
 source "$(dirname "$(realpath "$0")")/common-setup.sh"
