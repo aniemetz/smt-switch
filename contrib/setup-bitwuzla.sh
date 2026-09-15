29d007f54c52262de6645a1932b8658fbbd12bfc
Aina Niemetz <aina.niemetz@gmail.com>
2026-09-15 03:01:49 -0700

    Update Bitwuzla version.

--- contrib/setup-bitwuzla.sh
+++ contrib/setup-bitwuzla.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git_commit=122f27f9518269cd9c1ebf0efced37dfd7f845e6
+git_commit=ef068aa1c880a5cc7bfccf47679fbae20d300d2f
 
 prepare_step() {
   "$contrib_dir/setup-cadical.sh"
@@ -9,5 +9,8 @@ configure_step() {
   ./configure.py --prefix "$install_dir"
 }
 
+download_step() {
+  git clone --revision=$git_commit git@github.com:bitwuzla/bitwuzla-interpolants.git $dep_name
+}
 # shellcheck source=contrib/meson-setup.sh
 source "$(dirname "$(realpath "$0")")/meson-setup.sh"
